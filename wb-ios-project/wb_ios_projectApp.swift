2e340f7baff1bb89b2a40a264822f17430a602eb
biryukov.sergey71 <biryukov.sergey71@rwb.ru>
2026-06-30 18:07:00 +0300

    initial commit

---
+++ wb-ios-project/wb_ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  wb_ios_projectApp.swift
+//  wb-ios-project
+//
+//  Created by Sergei Biriukov on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct wb_ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
